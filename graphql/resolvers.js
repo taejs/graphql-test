25788c7d846ce605887c9664b5555e029683907e
tae <taeshindev@gmail.com>
2019-10-28 18:23:03 +0900

    #5 entity 정의

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,12 @@
 //query를 해결하는 방법에 대한 구현
+const teddy = {
+    name : "teddy",
+    age : 100,
+    IDE : "intelliJ"
+}
 const resolvers = {
     Query : {
-        name : ()=>'Teddy'
+        developer : ()=> teddy
     }
 }
 export default resolvers;
